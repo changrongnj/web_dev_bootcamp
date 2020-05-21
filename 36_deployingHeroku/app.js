3ef762893a79c50cdcf0f7242bce39f93d60a70b
Rong <changrongnj@gmail.com>
2020-05-21 00:06:07 -0700

    Add files via upload

---
+++ 36_deployingHeroku/app.js
@@ -0,0 +1,18 @@
+const express = require("express"),
+	  app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+	res.render("home");
+});
+
+app.get("/about", function(req, res) {
+	res.render("about");
+});
+
+
+app.listen(process.env.PORT || 3000, process.env.IP, function() {
+	console.log("The server started ...");
+});
+
---
+++ 36_deployingHeroku/readme.md
@@ -0,0 +1,9 @@
+# Heroku
+* `heroku login -i`
+* deploying app
+	* preparation: `git add [file]`
+	* `heroku create`
+	* `git remote -v`
+	* `git push heroku master`
+	* check error message `heroku log`
+	
